449d6b64b5cfeb2434a4125124900bf7291ce67c
Owais Yyameen <owais.yameen@hbl.com>
2025-04-05 15:39:05 +0500

    changes in payapl

--- routes/user/account/paymentMethod/paypalSuccess.js
+++ routes/user/account/paymentMethod/paypalSuccess.js
@@ -6,36 +6,39 @@ import getAccessToken from './accessToken.js';
 
 const paypalSuccess = async(req, res) => {
   try {
-    console.log(req,"testing");
-    
-    console.log(req.body,"boyd");
-    
-    console.log(req.query.token,"query");
-   // console.log(req.query.PayerID,"payer");
-   // console.log(req.query.paymentId,"payment");
-    
-    
-    // const payerId = req.query.PayerID;
-    // const paymentId = req.query.token;
+   
+const {token} = req.query
 
     const getToken = await getAccessToken()
-    // console.log(getToken,"getToke");
-// const orderId = "09F82665VX572742U"
-    const executeResponse = await axios.post(
-      `https://api-m.sandbox.paypal.com/v2/checkout/orders/${req.query.token}/capture`,
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${getToken}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.log("Payment Captured:", executeResponse.data);
+ 
+  // Step 1: AUTHORIZE the payment
+  const authorizeRes = await axios.post(
+    `https://api-m.sandbox.paypal.com/v2/checkout/orders/${token}/authorize`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${getToken}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  console.log("Payment Authorized:", authorizeRes.data);
+
+  // Optional: Save authorization ID to DB for later capture
+  const authorizationId =
+    authorizeRes.data.purchase_units[0].payments.authorizations[0].id;
+console.log(authorizationId,"authorizationId");
+
+  // Send minimal success response
+  //return res.send("<html><body style='background:#fff;'>Payment Authorized</body></html>");
+
+
+
 
     // const executeResponse = await axios.post(
-    //   `https://api-m.sandbox.paypal.com/v1/payments/payment/${paymentId}/execute`,
-    //   { payer_id: payerId },
+    //   `https://api-m.sandbox.paypal.com/v2/checkout/orders/${req.query.token}/capture`,
+    //   {},
     //   {
     //     headers: {
     //       Authorization: `Bearer ${getToken}`,
@@ -43,12 +46,15 @@ const paypalSuccess = async(req, res) => {
     //     },
     //   }
     // );
-    if(!executeResponse || executeResponse.length == 0){
-      res.redirect(
-        "http://3.110.42.187:5000/api/v1/user/account/payment/paypalcancel"
-      );
-    }
-    console.log("Payment Success:", executeResponse.data);
+    // console.log("Payment Captured:", executeResponse.data);
+
+  
+    // if(!executeResponse || executeResponse.length == 0){
+    //   res.redirect(
+    //     "http://localhost:5000/api/v1/user/account/payment/paypalcancel"
+    //   );
+    // }
+    // console.log("Payment Success:", executeResponse.data);
   return  res.send("<html><body style='background:#fff;'></body></html>");
 
     //return res.status(201).json({ status: 201, message: "Payment Success" });
